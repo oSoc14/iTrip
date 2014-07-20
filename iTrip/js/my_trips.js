6d4b80cc98ca8e6f1d9ce159aa03086aa56edf8c
Jasper De Rocker <jasper.derocker@gmail.com>
2014-07-20 19:59:34 +0200

    update addtrip page, second try

--- iTrip/js/my_trips.js
+++ iTrip/js/my_trips.js
@@ -7,6 +7,11 @@ function startDelete(e) {
             arrows[i].src = "images/placeholders/trash-43.png";
             arrows[i].alt = "trashcan icon";
         }
+        /*Delete images for every tripitem*/
+        var items = document.getElementsByClassName("resultInfo");
+        for (var j = 0; j < items.length; j++) {
+            items[j].style.setProperty("background-image", "url('images/placeholders/trash-43.png')");
+        }
     } else {
         e.src = "images/placeholders/placeholders-38.png";
         e.className = "";
@@ -14,6 +19,10 @@ function startDelete(e) {
         for (var i = 0; i < arrows.length; i++) {
             arrows[i].src = "images/placeholders/placeholders-39.png";
             arrows[i].alt = "arrow icon";
+        }
+        var items = document.getElementsByClassName("resultInfo");
+        for (var j = 0; j < items.length; j++) {
+            items[j].style.setProperty("background-image", "url('images/placeholders/placeholders-31.png')");
         }
     }
 }
@@ -33,7 +42,20 @@ function changeTrip(trip) {
     trip.className = "active";
     trip.getElementsByTagName("img")[0].src = "images/placeholders/placeholders-30.png";
     if (document.documentElement.clientWidth < 960) {
-        document.getElementById("resultSection").style.setProperty("display", "inherit", "important");
-        document.getElementById("searchPanel").style.setProperty("display", "none", "important");
+        document.getElementById("resultSection").style.setProperty("display", "initial");
+        document.getElementById("resultSection").style.setProperty("height", "100%");
+        document.getElementById("searchPanel").style.setProperty("display", "none");
     }
+}
+
+function toTravels() {
+    document.getElementById("resultSection").style.setProperty("display", "none");
+    document.getElementById("searchPanel").style.setProperty("height", "100%");
+    document.getElementById("searchPanel").style.setProperty("display", "initial");
+}
+
+function addmenu(e){
+    var numeric = e.id.replace('add', '');
+    var poi = document.getElementById("poi" + numeric);
+    poi.style.setProperty("visibility", "visible");
 }
